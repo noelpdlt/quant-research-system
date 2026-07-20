686a3c5408a613e41ac3175a6614603a89e96110
noelpdlt <155930828+noelpdlt@users.noreply.github.com>
2026-07-20 14:05:42 -0600

    Added 3 new features in features.py

--- src/features.py
+++ src/features.py
@@ -5,3 +5,28 @@ def compute_returns(df):
 def moving_average(df, window=10):
     df["ma"] = df["Close"].rolling(window).mean()
     return df
+
+def rolling_volatility(df, window=20):
+    df["volatility"] = (
+        df["returns"]
+        .rolling(window)
+        .std()
+    )
+    return df
+
+def volume_ratio(df, window=20):
+    df["volume_ratio"] = (
+        df["Volume"] /
+        df["Volume"].rolling(window).mean()
+    )
+    return df
+
+def trend_strength(df):
+    df["ma20"] = df["Close"].rolling(20).mean()
+    df["ma50"] = df["Close"].rolling(50).mean()
+
+    df["trend_strength"] = (
+        df["ma20"] - df["ma50"]
+    ) / df["ma50"]
+
+    return df
